269fb6ba65d0689a7cfbdb9c5a28ee6686e847c1
Adrian Foong <adrianfoong8@gmail.com>
2019-05-16 03:45:10 +0800

    commit

---
+++ app/src/main/java/com/example/csapp/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.csapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private ProgressBar progressBar;
+    private Button btnRegister, btnLogin, btnReset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
